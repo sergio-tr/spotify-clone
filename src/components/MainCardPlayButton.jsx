6a1eb86d64a1dbddde7beec933b9737d32a13db1
sergio-tr <truebanosergio@gmail.com>
2024-08-20 21:42:35 +0200

    Adding playlists' songs table with functionality and header without functionality

--- src/components/MainCardPlayButton.jsx
+++ src/components/MainCardPlayButton.jsx
@@ -1,30 +1,42 @@
 import { Pause, Play } from '@/components/Player'
 import { usePlayerStore } from '@/stores/playerStore'
 
-export function MainCardPlayButton({ id, size = 'small' }){
+export function MainCardPlayButton({ id, songNumber = -1, size = 'small', bgColor = 'bg-green-500', bgHoverColor = 'bg-green-400' }){
     const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } = usePlayerStore(state => state)
 
-    const isThisPlaying = isPlaying && currentMusic?.playlist?.id === id
+    let isThisPlaying = songNumber === -1 
+        ? isPlaying && currentMusic?.playlist?.id === id
+        : isPlaying && currentMusic?.playlist?.id === id && currentMusic?.song?.id-1 === songNumber
 
     const handleClick = () => {
 
         if(isThisPlaying){
-            setCurrentMusic({ })
+            setIsPlaying(false)
             return
         }
 
-        fetch(`/api/get-info-playlist.json?id=${id}`)
+        if(currentMusic?.playlist?.id !== id){
+            fetch(`/api/get-info-playlist.json?id=${id}`)
             .then(res => res.json())
             .then(data => {
                 const { songs, playlist } = data
 
-                setCurrentMusic({ songs, playlist, song: songs[0] })
+                setCurrentMusic({ songs, playlist, song: songs[songNumber === -1 ? 0 : songNumber] })
             })
+        } else {
+            setIsPlaying(true)
+        }
+
     }
 
-    const iconClassName = size === 'small' ? 'w-4 h-4' : 'w-6 h-6'
+    let iconClassName = size === 'small' ? 'w-4 h-4' : 'w-6 h-6'
 
-    return <button className="main-card-play-button rounded-full bg-green-500 p-4" onClick={handleClick}>
+    if(size === 'extra-small') iconClassName = 'w-3 h-3'
+
+    // FIXME: hover:bg-color does not seem to work
+    const buttonClassName = `main-card-play-button rounded-full ${bgColor} p-4 hover:scale-105 transition hover:${bgHoverColor}`
+
+    return <button className={buttonClassName} onClick={handleClick}>
         {isThisPlaying ? <Pause className={ iconClassName }/> : <Play className={ iconClassName } />}
     </button>
 }
--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -1,14 +1,29 @@
-export const colors = {
-  red: { bgColor: "bg-red-600", accent: "#da2735", dark: "#7f1d1d" },
-  orange: { bgColor: "bg-orange-600", accent: "#cc5400", dark: "#7c2d12" },
-  yellow: { bgColor: "bg-yellow-600", accent: "#ffae00", dark: "#78350f" },
-  green: { bgColor: "bg-green-600", accent: "#21c872", dark: "#14532d" },
-  teal: { bgColor: "bg-teal-600", accent: "#2ee9d7", dark: "#134e4a" },
-  blue: { bgColor: "bg-blue-600", accent: "#1e3a8a", dark: "#1e3a8a" },
-  indigo: { bgColor: "bg-indigo-600", accent: "#394bd5", dark: "#312e81" },
-  purple: { bgColor: "bg-purple-600", accent: "#df24ff", dark: "#581c87" },
-  pink: { bgColor: "bg-pink-600", accent: "#f33b73", dark: "#831843" },
-  emerald: { bgColor: "bg-emerald-600", accent: "#0c6e54", dark: "#064e3b" },
-  rose: { bgColor: "bg-rose-600", accent: "#ed2377", dark: "#871b48" },
-  gray: { bgColor: "bg-gray-600", accent: "#555555", dark: "#27272a" },
+export interface Color {
+  bgColor: string;
+  bgHoverColor: string;
+}
+
+export interface Colors {
+  [key: string]: Color;
+}
+
+export const greenColor = {
+  bgColor: "bg-green-600",
+  bgHoverColor: "bg-green-500"
+}
+
+export const colors: Colors = {
+  red: { bgColor: "bg-red-600", bgHoverColor: "bg-red-500" },
+  orange: { bgColor: "bg-orange-600", bgHoverColor: "bg-orange-500" },
+  yellow: { bgColor: "bg-yellow-600", bgHoverColor: "bg-yellow-500" },
+  green: greenColor,
+  teal: { bgColor: "bg-teal-600", bgHoverColor: "bg-teal-500" },
+  blue: { bgColor: "bg-blue-600", bgHoverColor: "bg-blue-500" },
+  indigo: { bgColor: "bg-indigo-600", bgHoverColor: "bg-indigo-500" },
+  purple: { bgColor: "bg-purple-600", bgHoverColor: "bg-purple-500" },
+  pink: { bgColor: "bg-pink-600", bgHoverColor: "bg-pink-500" },
+  emerald: { bgColor: "bg-emerald-600", bgHoverColor: "bg-emerald-500" },
+  rose: { bgColor: "bg-rose-600", bgHoverColor: "bg-rose-500" },
+  gray: { bgColor: "bg-gray-600", bgHoverColor: "bg-gray-500" },
 };
+
